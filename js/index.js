beabd8ad391a08bc743d72621a0c04312e42cd2e
mross707 <rossmatt60@gmail.com>
2021-03-04 17:35:44 -0500

    changes for 19

---
+++ js/index.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $("#img").hover(function(){
+      alert("images/Frame 2.png", style="width: 550px; height: 571.24px;");
+    },
+    function(){
+      alert("images/Frame 1.png", style="width: 550px; height: 571.24px;");
+    }); 
+  });
